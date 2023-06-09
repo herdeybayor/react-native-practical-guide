dbd27c650fbabd6be0172c044ce53c83bf165e8e
herdeybayor <shereefdeen4real@gmail.com>
2023-06-09 08:00:30 +0100

    inline styling

---
+++ 02-basics/working-with-core-components/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Another piece of text!</Text>
+            <Text style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}>Hello World!</Text>
+            <Button title="Click me!" />
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
