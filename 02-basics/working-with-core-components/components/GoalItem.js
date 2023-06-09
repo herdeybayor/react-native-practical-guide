e96639fadeafdcff1e9493ce5390cbb25d74f9b0
herdeybayor <shereefdeen4real@gmail.com>
2023-06-09 14:39:27 +0100

    feat: add styling to pressable component 🎉

--- 02-basics/working-with-core-components/components/GoalItem.js
+++ 02-basics/working-with-core-components/components/GoalItem.js
@@ -2,11 +2,11 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 
 const GoalInput = ({ goal, onDeleteItem }) => {
     return (
-        <Pressable onPress={onDeleteItem.bind(this, goal.id)}>
-            <View style={styles.listItem}>
+        <View style={styles.listItem}>
+            <Pressable android_ripple={{ color: "#210644" }} onPress={onDeleteItem.bind(this, goal.id)} style={({ pressed }) => pressed && styles.pressedItem}>
                 <Text style={styles.listItemText}>{goal.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     );
 };
 
@@ -15,11 +15,14 @@ export default GoalInput;
 const styles = StyleSheet.create({
     listItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: "#5e0acc",
     },
     listItemText: {
         color: "#ffffff",
+        padding: 8,
+    },
+    pressedItem: {
+        opacity: 0.5,
     },
 });
