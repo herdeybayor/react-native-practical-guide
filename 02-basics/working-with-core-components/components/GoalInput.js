b2cfc16a09cf9a32f27aea45dc05d58a439b580f
herdeybayor <shereefdeen4real@gmail.com>
2023-06-09 15:13:37 +0100

    chore: update styling ♻

--- 02-basics/working-with-core-components/components/GoalInput.js
+++ 02-basics/working-with-core-components/components/GoalInput.js
@@ -23,10 +23,10 @@ const GoalInput = ({ onAddGoal, visible, onCancel }) => {
                 <TextInput style={styles.textInput} placeholder="Your course goal!" onChangeText={goalInputHandler} value={enteredGoal} />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
-                        <Button title="Add Goal" onPress={addGoalHandler} />
+                        <Button title="Cancel" color="#f31282" onPress={onCancel} />
                     </View>
                     <View style={styles.button}>
-                        <Button title="Cancel" color="red" onPress={onCancel} />
+                        <Button title="Add Goal" color="#b180f0" onPress={addGoalHandler} />
                     </View>
                 </View>
             </View>
@@ -52,9 +52,12 @@ const styles = StyleSheet.create({
     },
     textInput: {
         borderWidth: 1,
-        borderColor: "#cccccc",
+        borderColor: "#e4d0ff",
+        backgroundColor: "#e4d0ff",
+        color: "#120438",
+        borderRadius: 6,
         width: "100%",
-        padding: 8,
+        padding: 16,
     },
     buttonContainer: {
         marginTop: 16,
