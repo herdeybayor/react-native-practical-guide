e619eff4f1b132452db6fa326de31d9f368aefbc
herdeybayor <shereefdeen4real@gmail.com>
2023-06-10 12:41:34 +0100

    chore: styling

--- 03-diving-deeper/guess_my_number/screens/StartGameScreen.js
+++ 03-diving-deeper/guess_my_number/screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import { PrimaryButton } from "../components";
 const StartGameScreen = () => {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput style={styles.numberInput} keyboardType="number-pad" maxLength={2} />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -26,5 +26,16 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.26,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#ddb52f",
+        borderBottomWidth: 2,
+        color: "#ddb52f",
+        marginVertical: 8,
+        fontWeight: "bold",
+        textAlign: "center",
     },
 });
