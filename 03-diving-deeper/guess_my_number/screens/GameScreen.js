a7c6fa107dcc9d664fcc5f001c2cc61f924f1da9
herdeybayor <shereefdeen4real@gmail.com>
2023-06-10 22:02:29 +0100

    feat: handle game logic 🎉

--- 03-diving-deeper/guess_my_number/screens/GameScreen.js
+++ 03-diving-deeper/guess_my_number/screens/GameScreen.js
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-import { NumberContainer, Title } from "../components";
+import { NumberContainer, PrimaryButton, Title } from "../components";
 
 const GameScreen = ({ userNumber }) => {
     const generateRandomBetween = React.useCallback((min, max, exclude) => {
@@ -16,10 +16,36 @@ const GameScreen = ({ userNumber }) => {
     }, []);
 
     const [currentGuess, setCurrentGuess] = React.useState(generateRandomBetween(1, 100, userNumber));
+
+    const nextGuessHandler = React.useCallback(
+        (direction) => {
+            if ((direction === "lower" && currentGuess < userNumber) || (direction === "greater" && currentGuess > userNumber)) {
+                Alert.alert("Don't lie!", "You know that this is wrong...", [{ text: "Sorry!", style: "cancel" }]);
+                return;
+            }
+            if (direction === "lower") {
+                const newMax = currentGuess - 1;
+                setCurrentGuess(generateRandomBetween(1, newMax, currentGuess));
+            } else {
+                const newMin = currentGuess + 1;
+                setCurrentGuess(generateRandomBetween(newMin, 100, currentGuess));
+            }
+        },
+        [currentGuess, userNumber]
+    );
+
     return (
         <View style={styles.screen}>
             <Title text="Opponent's Guess" />
             <NumberContainer number={currentGuess} />
+            <View>
+                <Text>Higher or Lower?</Text>
+                <View>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>-</PrimaryButton>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>+</PrimaryButton>
+                </View>
+            </View>
+            {/* <View>LOG ROUNDS</View> */}
         </View>
     );
 };
