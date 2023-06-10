a63ebd24e760552496a9449870bcfab13497fdaa
herdeybayor <shereefdeen4real@gmail.com>
2023-06-10 12:34:09 +0100

    chore: styling

--- 03-diving-deeper/guess_my_number/screens/StartGameScreen.js
+++ 03-diving-deeper/guess_my_number/screens/StartGameScreen.js
@@ -1,10 +1,10 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import React from "react";
 import { PrimaryButton } from "../components";
 
 const StartGameScreen = () => {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
@@ -13,3 +13,18 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        padding: 16,
+        backgroundColor: "#72063c",
+        elevation: 4,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+    },
+});
