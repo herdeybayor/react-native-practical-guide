6187bf816da91fdebb1a3df53e142f4b2f42f851
herdeybayor <shereefdeen4real@gmail.com>
2023-06-10 20:56:45 +0100

    feat: create title component

--- 03-diving-deeper/guess_my_number/screens/GameScreen.js
+++ 03-diving-deeper/guess_my_number/screens/GameScreen.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
+
+import { Title } from "../components";
 
 const GameScreen = ({ userNumber }) => {
     return (
         <View style={styles.screen}>
-            <Text>Opponent's Guess {userNumber}</Text>
+            <Title text="Opponent's Guess" />
         </View>
     );
 };
