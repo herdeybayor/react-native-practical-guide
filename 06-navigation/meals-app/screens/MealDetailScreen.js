8988870b90c0c47b3b8bd9ac2ae847cbe95186f4
herdeybayor <shereefdeen4real@gmail.com>
2023-07-15 03:37:26 +0100

    chore: update content ♻

--- 06-navigation/meals-app/screens/MealDetailScreen.js
+++ 06-navigation/meals-app/screens/MealDetailScreen.js
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
-const MealDetailScreen = () => {
+import { MEALS } from "../data/dummy-data";
+import { List, MealDetails, Subtitle } from "../components";
+
+function MealDetailScreen({ route }) {
+    const mealId = route.params.mealId;
+
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
     return (
-        <View>
-            <Text>MealDetailScreen</Text>
-        </View>
+        <ScrollView style={styles.rootContainer}>
+            <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+            <Text style={styles.title}>{selectedMeal.title}</Text>
+            <MealDetails duration={selectedMeal.duration} complexity={selectedMeal.complexity} affordability={selectedMeal.affordability} textStyle={styles.detailText} />
+            <View style={styles.listOuterContainer}>
+                <View style={styles.listContainer}>
+                    <Subtitle>Ingredients</Subtitle>
+                    <List data={selectedMeal.ingredients} />
+                    <Subtitle>Steps</Subtitle>
+                    <List data={selectedMeal.steps} />
+                </View>
+            </View>
+        </ScrollView>
     );
-};
+}
 
 export default MealDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 32,
+    },
+    image: {
+        width: "100%",
+        height: 350,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 24,
+        margin: 8,
+        textAlign: "center",
+        color: "white",
+    },
+    detailText: {
+        color: "white",
+    },
+    listOuterContainer: {
+        alignItems: "center",
+    },
+    listContainer: {
+        width: "80%",
+    },
+});
