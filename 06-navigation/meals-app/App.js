1978bdcaaf64d69bf34a6faa9a8bbac8ef2d66b4
herdeybayor <shereefdeen4real@gmail.com>
2023-07-15 04:37:48 +0100

    feat: add drawer 🎉

--- 06-navigation/meals-app/App.js
+++ 06-navigation/meals-app/App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-import { CategoryScreen, MealDetailScreen, MealsOverviewScreen } from "./screens";
+import { CategoryScreen, FavoritesScreen, MealDetailScreen, MealsOverviewScreen } from "./screens";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { CATEGORIES, MEALS } from "./data/dummy-data";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNav() {
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#351401",
+                },
+                headerTintColor: "#fff",
+                sceneContainerStyle: {
+                    backgroundColor: "#3f2f25",
+                },
+            }}
+        >
+            <Drawer.Screen name="Categories" component={CategoryScreen} />
+            <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+        </Drawer.Navigator>
+    );
+}
 
 export default function App() {
     return (
@@ -22,9 +42,16 @@ export default function App() {
                         contentStyle: {
                             backgroundColor: "#3f2f25",
                         },
+                        animation: "slide_from_right",
                     }}
                 >
-                    <Stack.Screen name="MealsCategories" component={CategoryScreen} />
+                    <Stack.Screen
+                        name="DrawerScreen"
+                        component={DrawerNav}
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
                     <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
                     <Stack.Screen name="MealDetails" component={MealDetailScreen} />
                 </Stack.Navigator>
